5eec234bfa6d69847123a556872886dd6df19c61
Ryan Li <ryli1998@bu.edu>
2020-03-30 14:10:15 -0400

    First commit

--- emptyIdleness/events/templates/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.utils import timezone
-
-class Event (models.Model):
-    eventID = models.CharField(max_length=20)
-    title = models.CharField(max_length = 120) #length of title
-    content = models.TextField()
-    date_posted = models.DateTimeField(default= timezone.now) #when post was created
-    
--- emptyIdleness/events/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import include, url
-from django.urls import path
-from . import views
-
-urlpatterns = [
-
-   path('', views.index, name = 'events-page'),
-    path('about/', views.about, name = 'events-about'),
-]
